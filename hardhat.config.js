f74a746d203a9b50e3a6c6baf0d7a5031c5e036c
Rohit Solia <rohit.solia@gmail.com>
2021-05-18 19:46:32 -0500

    Fix tests, remove todo

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,17 +8,6 @@ require('dotenv').config()
 
 const gasPrice = 55000000000
 
-task('accounts', 'Prints the list of accounts', async function () {
-  const accounts = await ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
-// TODO add deployment scripts
-// TODO add support for ledger for deployment
-// TODO use hardhat-deploy, I think that has support for ledger
-// TODO Figure out how to get fork work with expectRevert tests. I think this is hardhat bug
 module.exports = {
   defaultNetwork: 'hardhat',
   networks: {
