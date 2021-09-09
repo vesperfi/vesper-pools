0debbcf7bfcb9b5d39b106dd28597b6b78b8fc8e
Manoj <patidarmanoj@gmail.com>
2021-09-09 12:55:58 -0700

    Redeployed rari eth strategy

--- deploy/VAETH/02-VAETH-Rarifuse#23.js
+++ deploy/VAETH/02-VAETH-Rarifuse#23.js
@@ -1,16 +1,17 @@
 'use strict'
 
 const Address = require('../../helper/ethereum/address')
-const RariFuseStrategy = 'RariFuseStrategy'
+const RariFuseStrategy = 'RariFuseStrategyETH'
 const PoolAccountant = 'PoolAccountant'
 const {BigNumber} = require('ethers')
 const DECIMAL18 = BigNumber.from('1000000000000000000')
 const ONE_MILLION = DECIMAL18.mul('1000000')
 const RARI_FUSE_POOL_ID = 23
+const KEEPER = '0x76d266dfd3754f090488ae12f6bd115cd7e77ebd'
 const config = {
   feeCollector: Address.FEE_COLLECTOR,
   interestFee: '1500',
-  debtRatio: '0',
+  debtRatio: '1000',
   debtRate: ONE_MILLION.toString(),
   withdrawFee: 60,
 }
@@ -27,6 +28,7 @@ const deployFunction = async function ({getNamedAccounts, deployments}) {
   await execute(RariFuseStrategy, {from: deployer, log: true}, 'init', Address.ADDRESS_LIST_FACTORY)
   await execute(RariFuseStrategy, {from: deployer, log: true}, 'approveToken')
   await execute(RariFuseStrategy, {from: deployer, log: true}, 'updateFeeCollector', config.feeCollector)
+  await execute(RariFuseStrategy, {from: deployer, log: true}, 'addKeeper', KEEPER)
    // Add strategy in pool accountant
    await execute(
     PoolAccountant,
