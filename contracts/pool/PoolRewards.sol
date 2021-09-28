c9e406d5aa92b27843493c9da1907b774752eb5f
Manoj <patidarmanoj@gmail.com>
2021-09-28 15:27:16 -0700

    Updated pool rewards of vawbtc

--- contracts/pool/PoolRewards.sol
+++ contracts/pool/PoolRewards.sol
@@ -43,7 +43,7 @@ contract PoolRewardsStorage {
 
 /// @title Distribute rewards based on vesper pool balance and supply
 contract PoolRewards is Initializable, IPoolRewards, ReentrancyGuard, PoolRewardsStorage {
-    string public constant VERSION = "3.0.13";
+    string public constant VERSION = "3.0.14";
     using SafeERC20 for IERC20;
 
     /**
