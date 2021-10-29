120248640fc053296966bd267ace35582ea93ce3
Marcelo Morgado <cmarcelom@gmail.com>
2021-10-29 15:01:53 +0100

    Use block number for CI and skip vLINK tests

--- test/vlink/pool.js
+++ test/vlink/pool.js
@@ -4,7 +4,9 @@ const {prepareConfig} = require('./config')
 const {shouldBehaveLikePool} = require('../behavior/vesper-pool')
 const {shouldBehaveLikeMultiPool} = require('../behavior/vesper-multi-pool')
 
-describe('vLINK Pool', function () {
+// Note: Skipping since vLIKE uses C.R.E.A.M. strategy
+// See more: https://github.com/bloqpriv/vesper-pools-v3/issues/600
+describe.skip('vLINK Pool', function () {
   prepareConfig()
   shouldBehaveLikePool('vLINK', 'LINK')
   shouldBehaveLikeMultiPool('vLINK')
