310cf056f86185e37c4ee1494d0a64a28103e1df
Marcelo Morgado <cmarcelom@gmail.com>
2021-11-17 20:14:11 +0000

    Amend polygon/avalanche pools config

--- helper/avalanche/poolConfig.js
+++ helper/avalanche/poolConfig.js
@@ -2,8 +2,17 @@
 
 const Address = require('./address')
 
+const { ADDRESS_LIST_FACTORY: addressListFactory, FEE_COLLECTOR: feeCollector } = Address
+const withdrawFee = 60
+
 const PoolConfig = {
-  VDAI: { contractName: 'VPool', poolParams: ['vDAI Pool', 'vDAI', Address.DAI] },
+  VDAI: {
+    contractName: 'VPool',
+    poolParams: ['vDAI Pool', 'vDAI', Address.DAI],
+    addressListFactory,
+    withdrawFee,
+    feeCollector,
+  },
 }
 
 module.exports = Object.freeze(PoolConfig)
