fe609958b54b97463d958d14e5e8e3a8bdfdbe16
Antonio Pellegrino <info@kaos.company>
2022-01-07 23:11:34 +0100

    Convex D3 Pool Strategy config

---
+++ test/vfrax/crv-cvx-d3pool.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { prepareConfig } = require('./config')
+const { shouldBehaveLikePool } = require('../behavior/vesper-pool')
+const { shouldBehaveLikeStrategy } = require('../behavior/strategy')
+const { strategyConfig } = require('../utils/chains').getChainData()
+
+describe('vaFRAX Pool with ConvexD3PoolStrategy', function () {
+  const strategy = strategyConfig.ConvexD3PoolStrategyFRAX
+  strategy.config.debtRatio = 9000
+  const strategies = [strategy]
+  prepareConfig(strategies)
+
+  describe('Pool Tests', function () {
+    shouldBehaveLikePool('vaFRAX', 'FRAX')
+  })
+
+  describe.only('Strategy Tests', function () {
+    shouldBehaveLikeStrategy(0, strategies[0].type, strategies[0].contract)
+  })
+})
