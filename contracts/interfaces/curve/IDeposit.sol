e9e841aefb35e2b6c8090ae01513809632824023
Antonio Pellegrino <info@kaos.company>
2022-03-28 15:34:00 +0200

    Fixes #1046: Low-level external call does not check that it calls a contract
    Fixes #1044: Wrong token index in Convex4PoolStrategyMUSDPool::_depositToCurve
    
    Fixes #1040: Wrong logic for decimals of tokens in Convex4PoolStrategyMUSDPool

--- contracts/interfaces/curve/IDeposit.sol
+++ contracts/interfaces/curve/IDeposit.sol
@@ -15,5 +15,7 @@ interface IDeposit4x {
 
     function calc_withdraw_one_coin(uint256 _token_amount, int128 i) external view returns (uint256);
 
+    function calc_token_amount(uint256[4] memory _amounts, bool is_deposit) external view returns (uint256);
+
     function token() external view returns (address);
 }
