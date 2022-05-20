3d5a5acf3f1aa0319260ba7e70ce455f820e65d5
Virendra Patidar <virendra@bloq.com>
2022-05-20 14:53:58 +0400

    Added Benqi XY for vaWBTCe pool

---
+++ test/avalanche/benqi-xy/vawbtce.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const testRunner = require('../../utils/testRunner')
+
+describe('VAWBTCe Pool', function () {
+  testRunner('VAWBTCe', ['VesperBenqiXYStrategyWBTCe'], [{ debtRatio: 9000 }])
+})
